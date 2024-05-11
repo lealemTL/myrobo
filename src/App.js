f4af2b877cba44b068ed408cbb22861921bfcd5d
lealemTL <78681255+lealemTL@users.noreply.github.com>
2024-05-11 10:40:12 +0300

    version from my laptop

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CardList from "./CardList";
+import SearchBox from './SearchBox';
+import { robots } from "./robots";
+
+const App=()=>{
+    return (
+        <div className="tc">
+            <h1>My_Robo</h1>
+            <SearchBox />
+            <CardList robots={robots}/>
+        </div>
+    );
+}
+export default App;
