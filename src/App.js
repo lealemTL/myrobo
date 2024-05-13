a22a12f89b9f2350402aa01d684a0d2a2881ee3d
lealemTL <lealemgershom@gmail>
2024-05-13 16:45:24 +0300

    filtering is done

--- src/App.js
+++ src/App.js
@@ -3,21 +3,30 @@ import CardList from "./CardList";
 import SearchBox from './SearchBox';
 import { robots } from "./robots";
 
-const state = {
-    robots: robots,
-    searchfield: ''
-}
 class App extends Component{
     constructor(){
-        this.state
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    onSearchCange = (event)=>{
+
+        this.setState({searchfield:event.target.value})
+        
+
     }
     render(){
+        const filterRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
         return (
         <div className="tc">
             <h1>My_Robo</h1>
-            <SearchBox />
-            <CardList robots={robots}/> 
-        </div>
+            <SearchBox searchChange={this.onSearchCange}/>
+            <CardList robots={filterRobots}/> 
+        </div> 
     ); 
     }
     
