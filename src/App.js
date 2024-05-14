bc4a9851bbf70aeed87816451a091464b10b9a82
lealemTL <lealemgershom@gmail>
2024-05-14 11:50:54 +0300

    scroll is done

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
 import React ,{ Component }from "react";
 import CardList from "./CardList";
 import SearchBox from './SearchBox';
-import { robots } from "./robots";
+import Scroll from './Scroll';
+//import { robots } from "./robots";
+import './App.css';
 
 class App extends Component{
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users').then(
+            response=>{
+               return response.json();
+            }
+        ).then(users =>{
+            this.setState({robots:users})
+        })
+        
+    }
+
+
     onSearchCange = (event)=>{
 
         this.setState({searchfield:event.target.value})
-        
-
     }
     render(){
         const filterRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
+        if(this.state.robots.length ===0){
+            return <h1 className="f2 sega-font tc">Loading</h1>
+        }else{
+            return (
         <div className="tc">
-            <h1>My_Robo</h1>
+            <h1 className="f2 sega-font">My Robots</h1>
             <SearchBox searchChange={this.onSearchCange}/>
-            <CardList robots={filterRobots}/> 
+            <Scroll>
+                <CardList robots={filterRobots}/>
+            </Scroll>
+             
         </div> 
     ); 
+        }
+        
     }
-    
 }
 export default App;
